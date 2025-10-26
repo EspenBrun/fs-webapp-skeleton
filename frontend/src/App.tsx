46f5a03ec4b81a48930ad83ac4f5ab00403b5396
Espen Kirkesæther Brun <espen_brun@hotmail.com>
2025-10-26 08:24:23 +0100

    Wrap content in div with default styles and confirm tailwind setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1>Todo</h1>;
+  return (
+    <div className='flex min-h-screen w-full flex-col bg-gray-100 p-4 text-gray-800 dark:bg-slate-800 dark:text-white'>
+      <h1 className='text-3xl font-bold'>Todo</h1>
+    </div>
+  );
 }
